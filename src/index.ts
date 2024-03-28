675cdaf30893b5b88e2c0ae04946d667c83a8794
sudhirVoid <bhandarisudhir4274@gmail.com>
2024-03-28 22:25:05 +0530

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express!');
+});
+
+// Add this error handling middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
